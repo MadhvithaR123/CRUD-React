6e159eca46303a37aa18382a2ed85ee304c386a1
MadhvithaR123 <madhvitha.r12@gmail.com>
2023-11-16 20:12:20 +0530

    mom name

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       
       {/* <Spacer height="50px"/> */}
       <Navbar/>
-<p>madhvitha</p>
+<p>madhvitha rajeshwari</p>
       <Routes>
     <Route path="/" element={ <Login/> } />
     <Route path="signup" element={ <Signup/> } />
